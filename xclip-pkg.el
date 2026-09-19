f83da71c3b823aa713bc80cd75ac0542ec6069b4
agle <a.michael1@uq.edu.au>
2026-09-19 22:04:26 +1000

    verbmp

--- xclip-pkg.el
+++ xclip-pkg.el
@@ -1,2 +1,2 @@
 ;; Generated package description from xclip.el  -*- no-byte-compile: t -*-
-(define-package "xclip" "1.11.1.0.20240912.92814" "Copy&paste GUI clipboard from text terminal" 'nil :commit "7febe164de2a881b83b9d604d3c7cf20b69f422d" :url "https://elpa.gnu.org/packages/xclip.html" :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com") :keywords '("convenience" "tools"))
+(define-package "xclip" "1.11.2.0.20260912.92814" "Copy&paste GUI clipboard from text terminal" 'nil :commit "d9abad871662267303037ef04b2076b36ae71c64" :url "https://elpa.gnu.org/packages/xclip.html" :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com") :keywords '("convenience" "tools"))
